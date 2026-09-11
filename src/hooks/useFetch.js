1143b40536f8523dfda5af200f5e167f3c506921
minaps <m.pasbanpour@gmail.com>
2026-09-11 17:25:40 +0330

    refactor useFetch with useReducer

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -11,7 +11,6 @@ export default function useFetch(url) {
             case "FETCH_START":
                 return {
                     ...state,
-                    data: null,
                     isLoading: true,
                     error: null,
                 };
@@ -34,50 +33,44 @@ export default function useFetch(url) {
     const [state, dispatch] = useReducer(reducer, initialState);
     const requestRef = useRef(0);
     const controllerRef = useRef();
-
-    const timerRef = useRef();
     
     const fetchData = useCallback(() => {
         if(controllerRef.current) {
             controllerRef.current.abort();
         }
-        if(timerRef.current) {
-            clearTimeout(timerRef.current);
-        }
+
         const controller = new AbortController();
         dispatch({type: "FETCH_START"})
         const requestId = ++requestRef.current;
         controllerRef.current = controller;
-        timerRef.current = setTimeout(() => {
-            fetch(url, {signal: controller.signal})
-            .then(response => {
-                if( response.status === 404) {
-                    throw new Error(`${response.status} : Resource NOt Found`)
-                }
-                if(!response.ok) {
-                    throw new Error("Something went wrong !!!");
-                }
-                return response.json();
+        fetch(url, {signal: controller.signal})
+        .then(response => {
+            if( response.status === 404) {
+                throw new Error(`${response.status} : Resource NOt Found`)
+            }
+            if(!response.ok) {
+                throw new Error("Something went wrong !!!");
+            }
+            return response.json();
+        })
+        .then(json  => {
+            if (requestId !== requestRef.current) return;
+            dispatch({
+                type: "FETCH_SUCCESS",
+                payload: json,
             })
-            .then(json  => {
-                if (requestId !== requestRef.current) return;
+        })
+        .catch(error => {
+            if (requestId !== requestRef.current) return;
+            if(error.name==='AbortError') {
+                console.log('Fetch aborted');
+            } else {
                 dispatch({
-                    type: "FETCH_SUCCESS",
-                    payload: json,
+                    type: "FETCH_ERROR",
+                    payload: error.message,
                 })
-            })
-            .catch(error => {
-                if (requestId !== requestRef.current) return;
-                if(error.name==='AbortError') {
-                    console.log('Fetch aborted');
-                } else {
-                    dispatch({
-                        type: "FETCH_ERROR",
-                        payload: error.message,
-                    })
-                }
-            })
-        }, 1000);
+            }
+        })
     },[url]);
         
     useEffect(()=>{
@@ -85,9 +78,6 @@ export default function useFetch(url) {
         return () => {
             if (controllerRef.current) {
                 controllerRef.current.abort();
-            }
-            if (timerRef.current) {
-                clearTimeout(timerRef.current);
             }
         };
             
